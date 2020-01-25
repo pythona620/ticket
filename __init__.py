209fdc0598fb7ef1ce3582cb9fb24c97dd574c49
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-25 12:42:46 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
-class TestingSkill(MycroftSkill):
+class TicketSkill(MycroftSkill):
     def __init__(self):
+        
         super().__init__()
-
-    @intent_handler('what.is.a.prasad.intent')
-    def handle_what_is(self, message):
-        self.speak_dialog('prasad.description')
-
-    @intent_handler('prasad.you.like.intent')
-    def handle_do_you_like(self, message):
-        tomato_type = message.data.get('type')
-        if tomato_type is not None:
-            self.speak_dialog('like.tomato.type',
-                              {'type': tomato_type})
-        else:
-            self.speak_dialog('like.tomato.generic')
-
+        self.learning = True
+        
+    def initialize(self):
+        
+         @intent_handler(IntentBuilder('prasad.you.like.intent').require('prasad'))
+            
+            
+    def handle_thank_you_intent(self, message):
+      
+        self.speak_dialog("welcome")
+    
+    
     def stop(self):
         pass
-
-def create_skill():
+        
+def TicketSkill():
     return TestingSkill()
