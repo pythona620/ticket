e5ef0ef4e114dffc3e5ccd362a1584c355da9b23
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-25 15:00:10 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,23 +4,26 @@ class TomatoSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def initialize(self):
-        self.register_intent_file('what.is.a.prasad.intent', self.handle_what_is) #register the intentes
-        self.register_intent_file('prasad.you.like.intent', self.handle_do_you_like) #register the intentes
+#     def initialize(self):
+#         self.register_intent_file('what.is.a.prasad.intent', self.handle_what_is) #register the intentes
+#         self.register_intent_file('prasad.you.like.intent', self.handle_do_you_like) #register the intentes
 
     
-    def handle_what_is(self, message): 
-        self.speak('your friend is going to some place') #not given place
+#     def handle_what_is(self, message): 
+#         self.speak('your friend is going to some place') #not given place
 
-    def handle_do_you_like(self, message):
-        first_type = message.data.get('from')  #get the first keword
-        second_type = message.data.get('to') #get the second keword
+#     def handle_do_you_like(self, message):
+#         first_type = message.data.get('from')  #get the first keword
+#         second_type = message.data.get('to') #get the second keword
                      
-        if first_type is not None:
-            self.speak("your friend is going " + first_type  + " " + "to" + " "+  second_type ) #print specific keword
-            self.speak(first_type + " "+second_type)
-        else:
-            self.speak('you are not given any specific word!') #print if specific keword is not given
+#         if first_type is not None:
+#             self.speak("your friend is going " + first_type  + " " + "to" + " "+  second_type ) #print specific keword
+#             self.speak(first_type + " "+second_type)
+#         else:
+#             self.speak('you are not given any specific word!') #print if specific keword is not given
+    def initialize(self):
+        self.register_entity_file('from.entity')  # TODO: Keep?
+#         self.recurrence_dict = self.translate_namedvalues('recurring')
             
             
     def stop(self):
