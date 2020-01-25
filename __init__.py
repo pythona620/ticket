f912fb9329117c08f3c9d36822e18dab4016d3f2
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-25 12:46:10 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,26 @@
-from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from mycroft import MycroftSkill, intent_file_handler
 
-class TicketSkill(MycroftSkill):
+class TomatoSkill(MycroftSkill):
     def __init__(self):
-        
-        super().__init__()
-        self.learning = True
-        
+        MycroftSkill.__init__(self)
+
     def initialize(self):
-        
-         @intent_handler(IntentBuilder('prasad.you.like.intent').require('prasad'))
-            
-            
-    def handle_thank_you_intent(self, message):
-      
-        self.speak_dialog("welcome")
-    
-    
+        self.register_intent_file('what.is.a.prasad.intent', self.handle_what_is) #register the intentes
+        self.register_intent_file('prasad.you.like.intent', self.handle_do_you_like) #register the intentes
+
+    def handle_what_is(self, message): #get the intent
+        self.speak('A tomato is a big red thing') #
+
+    def handle_do_you_like(self, message):
+        tomato_type = message.data.get('type')  #get the specific keword into type
+        if tomato_type is not None:
+            self.speak("your friend is going " + tomato_type ) #print specific keword
+        else:
+            self.speak('you are not given any specific word!') #print if specific keword is not given
+
+
     def stop(self):
         pass
-        
-def TicketSkill():
-    return TestingSkill()
+
+def create_skill():
+    return TomatoSkill()
