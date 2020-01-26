259571341faaf3572f894784676567b837557a4d
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-26 19:10:38 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,17 +12,28 @@ class TicketSkill(MycroftSkill):
 	
 	@intent_handler(IntentBuilder("").require("journy").optionally("travel"))
 	def handle_start_game_intent(self, message):
-		self.speak_dialog("start.journy")
-
+# 		self.speak_dialog("start.journy")
 		# get source
-		source = self.get_names("get.source")
+# 		source = self.get_names("get.source")
 		# get destination
-		destination = self.get_names("get.destination")
-		
-		
-		answer = source +" " + "to" + " " + destination #adding names
-
-		self.speak_dialog("trvout",{"answer":answer}) #output
+# 		destination = self.get_names("get.destination")
+# 		answer = source +" " + "to" + " " + destination #adding names
+# 		self.speak_dialog("trvout",{"answer":answer}) #output
+		def  enter_source_destination(stops):
+		while True:
+			source = input('Enter your boarding bus stop: ')
+			if source in stops:
+				while True:
+					destination = input('Enter your destination: ')
+					if destination in stops:
+						return source, destination
+					else:
+					     print('Could you please enter a valid destination stop')
+					     continue
+ 
+	stops = ['vizag', 'hyderabad', 'vijayawada']
+	source, destination = enter_source_destination(stops)
+	print('The sourceing point is ', source, 'and the destination is ', destination)
 	def stop(self):		
 		pass
 def create_skill():
