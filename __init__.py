8011e5ef67b04aa5118a6d9e591e9bee7cde9b9e
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-27 10:46:17 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,30 +5,30 @@ class multipleentitiesSkill(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        self.register_intent_file('prasad.you.like.intent', self.handle_do_you_like) #register the intentes
+        self.register_intent_file('prasad.you.like.intent', self.handle_prasad_you_like) #register the intentes
 
     def handle_prasad_you_like(self, message):
         source = message.data.get('source')  #get the first keword
         destination = message.data.get('destination') #get the second keword
-                     
-    def  enter_source_destination(stops, message):
-    while True:
-        source = message.data.get('source')
-        if source in stops:
-            while True:
-                destination = message.data.get('destination')
-                if destination in stops:
-                    return source, destination
-                else:
-                    print('Could you please enter a valid destination stop')
-                    continue
-        else:
-            print('Could you please enter a valid boarding point')
-            continue
+        self.speak(first_type + " "+second_type)             
+#     def handle_enter_source_destination(stops, message):
+#     while True:
+#         source = message.data.get('source')
+#         if source in stops:
+#             while True:
+#                 destination = message.data.get('destination')
+#                 if destination in stops:
+#                     return source, destination
+#                 else:
+#                     print('Could you please enter a valid destination stop')
+#                     continue
+#         else:
+#             print('Could you please enter a valid boarding point')
+#             continue
 
-    stops = ['vizag', 'hyderabad', 'vijayawada']
-    source, destination = enter_source_destination(stops)
-    self.speak("The sourceing point is " + source + "and the destination is" +destination)
+#     stops = ['vizag', 'hyderabad', 'vijayawada']
+#     source, destination = enter_source_destination(stops)
+#     self.speak("The sourceing point is " + source + "and the destination is" +destination)
 
 
     def stop(self):
