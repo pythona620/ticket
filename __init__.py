e37dc551ff38dcddc76f377f356aa8ab8b842939
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-27 11:49:42 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-class multipleentitiesSkill(MycroftSkill):
+class TicketSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
@@ -11,28 +11,28 @@ class multipleentitiesSkill(MycroftSkill):
         source = message.data.get('source')  
         destination = message.data.get('destination')
     self.speak(source,destination)          
-    def handle_enter_source_destination(self, stops):
-        while == stops:
-            source = self.get_sourcebyuser("get.source")
-            if source in stops:
-                while True:
-                    destination = self.get_destinationbyuser("get.destination")
-                    if destination in stops:
-                        return source, destination
-                    else:
-                        self.speak('Could you please enter a valid destination stop')
-                        continue
-            else:
-                self.speak('Could you please enter a valid boarding point')
-                continue
+#     def handle_enter_source_destination(self, stops):
+#         while == stops:
+#             source = self.get_sourcebyuser("get.source")
+#             if source in stops:
+#                 while True:
+#                     destination = self.get_destinationbyuser("get.destination")
+#                     if destination in stops:
+#                         return source, destination
+#                     else:
+#                         self.speak('Could you please enter a valid destination stop')
+#                         continue
+#             else:
+#                 self.speak('Could you please enter a valid boarding point')
+#                 continue
 
-    self.speak("The sourceing point is " + source + "and the destination is" +destination)
+#     self.speak("The sourceing point is " + source + "and the destination is" +destination)
 
 
     def stop(self):
-        stops = ['vizag', 'hyderabad', 'vijayawada']
-        source, destination = enter_source_destination(stops)
+#         stops = ['vizag', 'hyderabad', 'vijayawada']
+#         source, destination = enter_source_destination(stops)
         pass
 
 def create_skill():
-    return multipleentitiesSkill()
+    return TicketSkill()
