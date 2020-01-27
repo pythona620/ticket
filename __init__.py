aeb666b667fe6b231092a2d1e15144c058b93acb
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-27 13:28:07 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,28 +10,27 @@ class TicketSkill(MycroftSkill):
     def handle_prasad_you_like(self, message):
         source = message.data.get('source')  
         destination = message.data.get('destination')
-        self.speak(source,destination)          
-#     def handle_enter_source_destination(self, stops):
-#         while == stops:
-#             source = self.get_sourcebyuser("get.source")
-#             if source in stops:
-#                 while True:
-#                     destination = self.get_destinationbyuser("get.destination")
-#                     if destination in stops:
-#                         return source, destination
-#                     else:
-#                         self.speak('Could you please enter a valid destination stop')
-#                         continue
-#             else:
-#                 self.speak('Could you please enter a valid boarding point')
-#                 continue
+        self.speak (source,destination)          
+    def handle_enter_source_destination(self, stops):
+        while True:
+            source = self.get_sourcebyuser("get.source")
+            if source in stops:
+                while True:
+                    destination = self.get_destinationbyuser("get.destination")
+                    if destination in stops:
+                        return source, destination
+                    else:
+                        self.speak('Could you please enter a valid destination stop')
+                        continue
+            else:
+                self.speak('Could you please enter a valid boarding point')
+                continue
 
-#     self.speak("The sourceing point is " + source + "and the destination is" +destination)
-
+    self.speak("The source point is " + source + "and the destination is" +destination)
+    source, destination = enter_source_destination(stops)
 
     def stop(self):
-#         stops = ['vizag', 'hyderabad', 'vijayawada']
-#         source, destination = enter_source_destination(stops)
+        stops = ['vizag', 'hyderabad', 'vijayawada']
         pass
 
 def create_skill():
