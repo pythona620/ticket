7aaa106353f678f8a6eae0ff226405ee5332f6f2
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-31 11:39:40 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,34 +5,34 @@ from random import randint
 __author__ = 'python'
 LOGGER = getLogger(__name__)
 class ticketSkill(MycroftSkill):
-	def get_stops(self, dialog):
+	def get_numerical_response(self, dialog):
 		yip = self.get_response(dialog) 
 		return yip
 	@intent_handler(IntentBuilder("").require("travel").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
 		# get lower bound
-	lowerBound = input("get.lower")
+	lowerBound = self.get_numerical_response("get.lower")
 		# get myfriendname
-	upperBound = input("get.upper")
-	def  enter_source_destination(self,stops):
-    	while True:
-        	source = lowerBound
-        	if source in stops:
-            	while True:
-                	destination = upperBound
-                	if destination in stops:
-                    		return source, destination
-                	else:
-                    		self.speak('Could you please enter a valid destination stop')
-                    		continue
-        	else:
-            		self.speak('Could you please enter a valid boarding point')
-            		continue
+	upperBound = self.get_numerical_response("get.upper")
+# 	def  enter_source_destination(self,stops):
+#     	while True:
+#         	source = lowerBound
+#         	if source in stops:
+#             	while True:
+#                 	destination = upperBound
+#                 	if destination in stops:
+#                     		return source, destination
+#                 	else:
+#                     		self.speak('Could you please enter a valid destination stop')
+#                     		continue
+#         	else:
+#             		self.speak('Could you please enter a valid boarding point')
+#             		continue
 
-    	stops = ['vizag', 'hyderabad', 'vijayawada']
-    	source, destination = enter_source_destination(stops)
-    	self.speak('The sourceing point is ', source, 'and the destination is ', destination)
+#     	stops = ['vizag', 'hyderabad', 'vijayawada']
+#     	source, destination = enter_source_destination(stops)
+#     	self.speak('The sourceing point is ', source, 'and the destination is ', destination)
 	
 	def stop(self):
 		pass
