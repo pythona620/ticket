8b1426f64a4136be0edb8a49a686c1e6d4b2ffe2
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-31 11:10:28 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,36 +1,41 @@
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG, getLogger
+from random import randint
+__author__ = 'python'
+LOGGER = getLogger(__name__)
+class checkingSkill(MycroftSkill):
+	def get_stops(self, dialog):
+		yip = self.get_response(dialog) 
+		return yip
+	@intent_handler(IntentBuilder("").require("travel").optionally("Play").optionally("Suggest"))
+	def handle_start_game_intent(self, message):
+		self.speak_dialog("start.game")
+		# get lower bound
+	lowerBound = input("get.lower")
+		# get myfriendname
+	upperBound = input("get.upper")
+	def  enter_source_destination(stops):
+    	while True:
+        	source = lowerBound
+        	if source in stops:
+            	while True:
+                	destination = upperBound
+                	if destination in stops:
+                    	return source, destination
+                	else:
+                    	self.speak('Could you please enter a valid destination stop')
+                    	continue
+        	else:
+            	self.speak('Could you please enter a valid boarding point')
+            	continue
 
-class TicketSkill(MycroftSkill):
-
-    def __init__(self):
-        super(TicketSkill,self).__init__()
-        stops = ["hyderabad","visakhapatnum","vijayawada","tirupathi","rajhamundri"]
-    def initialize(self):
-        self.register_entity_file('source.entity')
-        self.register_entity_file('destination.entity')
-
-    @intent_handler(IntentBuilder("").require("journy").optionally("travel"))
-
-    def plan_tour(self,sour,dest):
-        while true:
-            source = self.get_sourcebyuser("get.sour")
-            if source in stops:
-                while true:
-                    destination = self.get_destinationbyuser("get.dest")
-                    if destination in stops:
-                        return sour, dest
-                    else:
-                        self.speak('Could you please enter a valid destination stop')
-                        continue
-            else:
-                self.speak('Could you please enter a valid boarding point')
-                continue
-
-    self.speak("The source point is " + source + "and the destination is" + destination)
-    def stop(self):
-        
-        pass
-
+    stops = ['vizag', 'hyderabad', 'vijayawada']
+    source, destination = enter_source_destination(stops)
+    self.speak('The sourceing point is ', source, 'and the destination is ', destination)
+	
+	def stop(self):
+		pass
+	
 def create_skill():
-    return TicketSkill()
+	return checkingSkill()
